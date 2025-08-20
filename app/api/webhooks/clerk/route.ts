210f95a1a71f3e2c406d4752c73d8052380463a1
Usman <testtest.@gmail.com>
2025-08-20 18:59:29 +0500

    Updated webhooks route file

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -2,6 +2,53 @@ import { verifyWebhook } from '@clerk/nextjs/webhooks'
 import { NextRequest } from 'next/server'
 import { db } from '@/lib/db'
 
+// export async function POST(req: NextRequest) {
+// 	try {
+// 		const evt = await verifyWebhook(req)
+
+// 		// Do something with payload
+// 		// For this guide, log payload to console
+// 		const { id } = evt.data
+// 		const eventType = evt.type
+// 		console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
+// 		console.log('Webhook payload:', evt.data)
+
+// 		if (eventType === 'user.created') {
+// 			await db.user.create({
+// 				data: {
+// 					clerkId: evt.data.id,
+// 					username: evt.data.username!,
+// 					avatar: evt.data.image_url,
+// 					fullName: `${evt.data.first_name} ${evt.data.last_name}`,
+// 					bio: 'Bio is not provided !!!',
+// 				},
+// 			})
+// 		}
+
+// 		if (eventType === 'user.updated') {
+// 			await db.user.update({
+// 				where: { clerkId: evt.data.id },
+// 				data: {
+// 					username: evt.data.username!,
+// 					avatar: evt.data.image_url,
+// 					fullName: `${evt.data.first_name} ${evt.data.last_name}`,
+// 				},
+// 			})
+// 		}
+
+// 		if (eventType === 'user.deleted') {
+// 			await db.user.delete({
+// 				where: { clerkId: evt.data.id },
+// 			})
+// 		}
+
+// 		return new Response('Webhook received', { status: 200 })
+// 	} catch (err) {
+// 		console.error('Error verifying webhook:', err)
+// 		return new Response('Error verifying webhook', { status: 400 })
+// 	}
+// }
+
 export async function POST(req: NextRequest) {
 	try {
 		const evt = await verifyWebhook(req)
@@ -13,33 +60,8 @@ export async function POST(req: NextRequest) {
 		console.log(`Received webhook with ID ${id} and event type of ${eventType}`)
 		console.log('Webhook payload:', evt.data)
 
-		if (eventType === 'user.created') {
-			await db.user.create({
-				data: {
-					clerkId: evt.data.id,
-					username: evt.data.username!,
-					avatar: evt.data.image_url,
-					fullName: `${evt.data.first_name} ${evt.data.last_name}`,
-					bio: 'Bio is not provided !!!',
-				},
-			})
-		}
-
-		if (eventType === 'user.updated') {
-			await db.user.update({
-				where: { clerkId: evt.data.id },
-				data: {
-					username: evt.data.username!,
-					avatar: evt.data.image_url,
-					fullName: `${evt.data.first_name} ${evt.data.last_name}`,
-				},
-			})
-		}
-
-		if (eventType === 'user.deleted') {
-			await db.user.delete({
-				where: { clerkId: evt.data.id },
-			})
+		if (evt.type === 'user.created') {
+			console.log('userId:', evt.data.id)
 		}
 
 		return new Response('Webhook received', { status: 200 })
